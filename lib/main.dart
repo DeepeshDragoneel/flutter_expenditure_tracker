6cbeca8cdac718f75574bd9def170c40e93e4d82
DeepeshDragoneel <deepeshash444@gmail.com>
2021-10-17 15:36:32 +0530

    Initialized flutter

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Expense Tracker"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Card(
+              child: Container(width: 100, child: Text("CHART")),
+              elevation: 5,
+            ),
+            Card(
+              child: Text("EXPENSE List"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
